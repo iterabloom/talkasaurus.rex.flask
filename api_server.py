7e59e51b7632f62ca59cc647c1fe4bd6b50cbddf
josh-iterabloom <144993580+josh-iterabloom@users.noreply.github.com>
2023-09-15 16:07:31 +0000

    Improved server-side speech-to-text handling, added text-to-speech functionality and implemented necessary socket.io configuration in dockerfile and frontend

--- api_server.py
+++ api_server.py
@@ -6,6 +6,7 @@ import os
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 from google.cloud import speech_v1p1beta1 as speech
+from google.cloud import texttospeech
 
 
 #This would require an audio stream chunked into parts to work most effectively, which would require changes based on the specific implementation of audio input.
@@ -30,6 +31,28 @@ def transcribe_audio_stream(stream):
     for response in responses:
     	pass
         # Process responses
+
+
+def convert_text_to_speech(text):
+    client = texttospeech.TextToSpeechClient()
+
+    synthesis_input = texttospeech.SynthesisInput({"text": text})
+
+    # More voice settings can be added for better speech synthesis
+    voice = texttospeech.VoiceSelectionParams(language_code="en-US", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL)
+
+    # Audio config settings can be updated for better audio output
+    audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
+
+    # Creating the speech synthesis response
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+
+    # Save the synthesized speech to a file
+    with open("output.mp3", "wb") as out:
+        out.write(response.audio_content)
+    print("Audio content written to file output.mp3")
 
 
 app = Flask(__name__, static_folder='talkasaurus-react/build')
--- talkasaurus-react/src/App.js
+++ talkasaurus-react/src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
 
+let socket = io("http://localhost:8080");    // connect this to the server
+socket.on("connect", () => {});
+
+function handleAudioFile(file) {
+    let audio = new Audio(file);
+    audio.play();
+}
 function App() {
   return (
     <div className="App">
