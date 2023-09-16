4532eb6773322aa18d2c0380425a3abe27db3777
josh-iterabloom <144993580+josh-iterabloom@users.noreply.github.com>
2023-09-16 05:28:17 +0000

    test: Added basic unit tests for the application

--- tests/testing_script_1.py
+++ tests/testing_script_1.py
@@ -1,1 +1,24 @@
-# TODO: write this script to facilitate automated testing reports, which can send build notifications
+import unittest
+from flask_socketio import SocketIO, emit
+from flask import Flask
+from unittest.mock import Mock
+import api_server
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.socketio = SocketIO(self.app)
+        self.client = self.app.test_client()
+
+    def test_socket_connection(self):
+        mock = Mock()
+        self.socketio.on('connect', mock)
+        self.socketio.test_client(self.app).emit('connect')
+        assert mock.called
+
+    def test_generate_ai_response(self):
+        response = api_server.generate_ai_response('Hello')
+        self.assertIsNotNone(response)
+
+if __name__ == '__main__':
+    unittest.main()
