a1a3eec89094715e2c59be98d7d90602e3c5d6e0
josh-iterabloom <144993580+josh-iterabloom@users.noreply.github.com>
2023-09-16 04:33:41 +0000

    feat: allow dynamic setting of socket server URL from environment variables

--- talkasaurus-react/src/SocketContextPanel.js
+++ talkasaurus-react/src/SocketContextPanel.js
@@ -4,11 +4,13 @@ import io from "socket.io-client";
 const SocketContext = React.createContext();
 
 export const SocketProvider = ({ children }) => {
-  // initialising socket connection
-  const socket = io(process.env.REACT_APP_SERVER_URL, {
-    transports: ["websocket"],
-    upgrade: false,
-  });
+  const socketURL = process.env.REACT_APP_SERVER_URL;
+
+  const [socket, setSocket] = useState()
+
+  useEffect(() => {
+    setSocket(io(socketURL));
+  }, [socketURL])
 
   return (
     <SocketContext.Provider value={socket}>
